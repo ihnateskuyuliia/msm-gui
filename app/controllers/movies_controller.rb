402a62952e3d7e883e27a2720f0e128d8656b60f
ihnateskuyuliia <yuliiaihnatesku@uchicago.edu>
2024-11-11 16:48:20 +0000

    movie form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,5 @@
 class MoviesController < ApplicationController
 
-def create
-# retrieve the user's inputs from params
-end 
-
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
@@ -19,4 +15,22 @@ end
 
     render({ :template => "movie_templates/show" })
   end
+
+  def create
+    # retrieve the user's inputs from params
+    # params hash looks like this: 
+    # {"the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"6"}
+    
+      new_movie = Movie.all.new
+      new_movie.title = params.fetch("the_title")
+      new_movie.year = params.fetch("the_year")
+      new_movie.duration = params.fetch("the_duration")
+      new_movie.description = params.fetch("the_description")
+      new_movie.image = params.fetch("the_image")
+      new_movie.director_id = params.fetch("the_director_id")
+    
+      new_movie.save
+      
+      redirect_to("/movies")
+    end 
 end
